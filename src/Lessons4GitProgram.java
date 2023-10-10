8c9229eaf0c08942f32f30cb9030c4ad242bd1a9
kvs23031999 <kvs23031999@gmail.com>
2023-10-10 21:40:59 +0300

    Add new system out println

--- src/Lessons4GitProgram.java
+++ src/Lessons4GitProgram.java
@@ -3,5 +3,6 @@ public class Lessons4GitProgram {
     public static void main(String[] args){
 
         System.out.println("Hello world and Git! iam Vladyslav");
+        System.out.println("Hello again");
     }
 }
